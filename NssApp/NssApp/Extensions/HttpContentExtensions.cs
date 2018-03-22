8f63cca754462f0384c1ada22ce9a4cba883da4f
Richard Godfrey <richjgodfrey@hotmail.com>
2018-03-22 16:06:07 +0000

    addded extension class

---
+++ NssApp/NssApp/Extensions/HttpContentExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Net.Http
+{
+    public static class HttpContentExtensions
+    {
+        public static async Task<T> FromJson<T>(this HttpContent httpContent)
+        {
+            var stringContent = await httpContent.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<T>(stringContent);
+        }
+    }
+}
