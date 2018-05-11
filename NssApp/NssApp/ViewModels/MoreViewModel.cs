238f8acf60660d637f51e26e5cd32c1d7505b4bc
Craig Hanson <craig.hanson@biomni.com>
2018-05-11 20:48:43 +0100

    Added More view so we can logout

---
+++ NssApp/NssApp/ViewModels/MoreViewModel.cs
@@ -0,0 +1,43 @@
+﻿using NssApp.RestApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NssApp.ViewModels
+{
+    public class MoreViewModel
+    {
+        private readonly LoggedInUserService _loggedInUserService;
+
+        public MoreViewModel(LoggedInUserService loggedInUserService)
+        {
+            this._loggedInUserService = loggedInUserService;
+        }
+
+        public MoreViewModel Initialize(Page page)
+        {
+            return this;
+        }
+        
+        public ICommand LogoutCommand { get => new Command(async () => await Logout()); }
+
+        private async Task Logout()
+        {
+            var x = await _loggedInUserService.TryGetLoggedInUserSettings();
+
+            _loggedInUserService.SetCredentials(new LoginSettings
+            {
+                BaseUrl = x.BaseUrl,
+                Username = "",
+                Password = "",
+                AccessToken = ""
+            });
+
+            Application.Current.MainPage = new SplashScreen();
+        }
+    }
+}
